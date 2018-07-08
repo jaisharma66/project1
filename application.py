8fdf5ab005f9f36f2eb1a8e0181d225692505ff5
Jai Sharma <jsharma19@lausanneschool.com>
2018-07-08 15:15:59 +0000

    pre-login

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +23,28 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/registered", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    db.execute("INSERT INTO users (name, username, password) VALUES (:name, :username, :password)",
+        {"name": name, "username": username, "password": password})
+    db.commit()
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    user_login = request.form.get("user_login")
+    pass_login = request.form.get("pass_login")
+    try:
+        db.execute("SELECT * FROM users WHERE ('username' = 'user_login') AND ('password' = 'pass_login')")
+        print("SUCC")
+    except ValueError:
+        print("Item Does Not Exist")
+
+
+if __name__ == "__main__":
+    index();
